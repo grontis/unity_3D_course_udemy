588048d0388297b027b0575d9babd7f14722c0c1
Grant Kveton <gkveton2017@fau.edu>
2018-11-12 22:04:17 -0500

    Lecture 90

--- Argon_Assault/Assets/player.cs
+++ Argon_Assault/Assets/player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
@@ -6,11 +7,20 @@ using UnityStandardAssets.CrossPlatformInput;
 public class player : MonoBehaviour {
 
 
-    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 4f; //4 meters/second
+    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 20f; //4 meters/second
     [Tooltip("In m")]    [SerializeField] float xRange = 5f; //4 meters/second
 
-    [Tooltip("In ms^-1")] [SerializeField] float ySpeed = 4f; //4 meters/second
-    [Tooltip("In m")] [SerializeField] float yRange = 5f; //4 meters/second
+    [Tooltip("In ms^-1")] [SerializeField] float ySpeed = 20f; //4 meters/second
+    [Tooltip("In m")] [SerializeField] float yRange = 3.5f; //4 meters/second
+
+    [SerializeField] float positionPitchFactor = -5f;
+    [SerializeField] float controlPitchFactor = -10f;
+
+    [SerializeField] float positionYawFactor = 5f;
+
+    [SerializeField] float controlRollFactor = 5f;
+
+    float xThrow, yThrow;
 
 
     // Use this for initialization
@@ -20,6 +30,26 @@ public class player : MonoBehaviour {
 
     // Update is called once per frame
     void Update()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+
+    }
+
+    private void ProcessRotation()
+    {
+        float pitchDueToPosition = this.transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+
+        float yaw = this.transform.localPosition.x * positionYawFactor;
+
+        float roll = xThrow * controlRollFactor;
+
+        this.transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation()
     {
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         float xOffset = xThrow * xSpeed * Time.deltaTime;
@@ -35,7 +65,9 @@ public class player : MonoBehaviour {
             this.transform.localPosition.z);
 
         float rawYPosition = transform.localPosition.y + yOffset;
-        // // 
-
+        float clampedYPosition = Mathf.Clamp(rawYPosition, -yRange, yRange);
+        transform.localPosition = new Vector3(this.transform.localPosition.x,
+            clampedYPosition,
+            this.transform.localPosition.z);
     }
 }
