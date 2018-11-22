3e2d31e83ff9c65467e14bf29c756ed613919aa2
Grant Kveton <gkveton2017@fau.edu>
2018-11-22 01:20:24 -0500

    Lecture 95
    Created a levellloader script,  was using the music player script to load levels before

---
+++ Argon_Assault/Assets/scripts/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        Invoke("LoadFirstScene", 4); //calls the LoadFirstScene after 4sec
+	}
+	
+	// Update is called once per frame
+	void LoadFirstScene () {
+        SceneManager.LoadScene(1);
+	}
+}
--- Argon_Assault/Assets/scripts/musicPlayer.cs
+++ Argon_Assault/Assets/scripts/musicPlayer.cs
@@ -1,22 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class musicPlayer : MonoBehaviour {
-
     private void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
     }
-
-    // Use this for initialization
-    void Start () {
-        Invoke("LoadFirstScene", 4); //calls the LoadFirstScene after 4sec
-	}
-	
-	// Update is called once per frame
-	void LoadFirstScene () {
-        SceneManager.LoadScene(1);
-	}
 }
