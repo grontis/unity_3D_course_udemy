0a361a32dac0c4d14d1eae61219ffe6a13386011
Grant Kveton <gkveton2017@fau.edu>
2018-11-23 03:47:26 -0500

    Lecture 102
    Enemy Explosion Instantiation

---
+++ Argon_Assault/Assets/scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Enemy : MonoBehaviour
+{
+
+	[SerializeField] private GameObject _deathFx;
+	[FormerlySerializedAs("_parent")] [SerializeField] private Transform _fxParent;
+	
+	
+	private void Start()
+	{
+		Collider nonTriggerCollider = gameObject.AddComponent<BoxCollider>();
+		nonTriggerCollider.isTrigger = false;
+	}
+
+	private void OnParticleCollision(GameObject other)
+	{
+		GameObject fx = Instantiate(_deathFx, transform.position, Quaternion.identity);
+		fx.transform.parent = _fxParent;
+		Destroy(gameObject);
+		Destroy(fx, 1.5f);
+	}
+}
