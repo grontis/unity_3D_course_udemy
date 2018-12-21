fb0a45a3d1e16d6a62eae6dbe23f0beada0cd55e
Grant Kveton <grantkveton@msn.com>
2018-12-20 21:08:58 -0500

    Lecture 144

---
+++ Realm_Rush/Assets/EnemyDamage.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    [SerializeField] private Collider collisionMesh;
+    [SerializeField] private int hitPoints = 10;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        if (hitPoints <= 1)
+        {
+            KillEnemy();
+        }
+    }
+
+    private void ProcessHit()
+    {
+        hitPoints = hitPoints - 1; /*TODO change the -1 to be a
+                                   variable that allows for different damage from different towers*/
+        print("Current hitpoints is " + hitPoints);
+    }
+
+    private void KillEnemy()
+    {
+            Destroy(gameObject);
+    }
+}
--- Realm_Rush/Assets/Scripts/Tower.cs
+++ Realm_Rush/Assets/Scripts/Tower.cs
@@ -11,7 +11,7 @@ public class Tower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        objectToPan.LookAt(targetEnemy);
     }
     
     
